11a47aa06748a02c980601964b581c2777eb583e
Gabriel <bielbobrov@hotmail.com>
2021-01-18 18:27:34 -0300

    bluetasks

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+
+
+export const APP_NAME = "BlueTasks";
+export const AUTH_ENDPOINT = "http://localhost:8080"
+export const API_ENDPOINT = "http://localhost:8080/api" 
+export const JWT_TOKEN_NAME = "jwt";
